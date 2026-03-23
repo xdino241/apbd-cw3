34f60f2bd15c3e2fc96a8c96ce0bd62f505fa70b
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-23 14:33:51 +0100

    Added new class Device.cs

---
+++ s33546/Device.cs
@@ -0,0 +1,14 @@
+﻿namespace s33546;
+
+public abstract class Device
+{
+    private static int _nextId = 1;
+    public int Id {get; private set;}
+    public string Name {get; set;}
+    
+    protected  Device(string name)
+    {
+        Id = _nextId++;
+        Name = name;
+    }
+}
