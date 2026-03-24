32561b2ebdb9e3bfa8be07e3eace6809670576e7
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-24 17:57:34 +0100

    Added new class Laptop.cs

---
+++ s33546/Laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace s33546;
+
+public class Laptop : Device
+{
+    public string Model {get; set;}
+    public int RAM {get; set;}
+    public string CPU {get; set;}
+    public Laptop(string model, int ram, string cpu) : base("Laptop")
+    {
+        Model = model;
+        RAM = ram;
+        CPU = cpu;
+    }
+}
