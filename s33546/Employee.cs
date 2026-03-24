b040106359a01bde68711a8ce724fa1d0f86be4d
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-24 18:37:26 +0100

    Added Employee class

---
+++ s33546/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace s33546;
+
+public class Employee : User
+{
+    public Employee(string firstName, string lastName, string userType, string position) : base(firstName, lastName, "Employee")
+    {
+        Position = position;
+    }
+
+    public string Position { get; set; }
+    
+}
