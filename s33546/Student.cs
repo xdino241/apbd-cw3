775f9209eca3d50d62be5abf8fd1c094fe2309dc
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-24 18:32:58 +0100

    Added class Student

---
+++ s33546/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace s33546;
+
+public class Student : User
+{
+    public Student(string firstName, string lastName, string userType, string fieldOfStudy) : base(firstName, lastName, "Student")
+    {
+        FieldOfStudy = fieldOfStudy;
+    }
+
+    public string FieldOfStudy { get; set; }
+    
+}
