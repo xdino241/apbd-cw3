87d0a4db10939afa5b38453ca219f1e8209262bf
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-24 18:31:33 +0100

    Added User class

---
+++ s33546/User.cs
@@ -0,0 +1,18 @@
+﻿namespace s33546;
+
+public abstract class User
+{
+    private static int _nextId = 1;
+    public int Id {get; private set;}
+    public string FirstName {get; set;}
+    public string LastName { get; set; }
+    public string UserType { get; protected set; }
+
+    protected User(string firstName, string lastName, string userType)
+    {
+        Id = _nextId++;
+        FirstName = firstName;
+        LastName = lastName;
+        UserType = userType;
+    }
+}
