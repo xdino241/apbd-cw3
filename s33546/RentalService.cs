abe8ca0f3895e09e4824b2377c70dd2f4067d5e3
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-24 20:06:50 +0100

    Added RentDevice

--- s33546/RentalService.cs
+++ s33546/RentalService.cs
@@ -2,13 +2,32 @@
 
 public class RentalService
 {
-    public List<Device> Devices = new List<Device>();
+    private List<Device> devicesToRent = new List<Device>();
+    private List<Rental> rentedDevices = new List<Rental>();
 
-    public void addDevice(Device device)
+    public void AddDevice(Device device)
     {
-        Devices.Add(device);
-        Console.WriteLine("Dodano do systemu: " +  device.Name);
+        devicesToRent.Add(device);
+        Console.WriteLine("Dodano do systemu: " +  device.Name + ", o modelu: " +  device.Model);
     }
-    
+
+    public void RentDevice(User user, Device device, DateTime dateFrom, DateTime dateTo)
+    {
+        if (!device.IsAvailable)
+        {
+            Console.WriteLine("BŁĄD: " + device.Name + ", o modelu: " + device.Model + " jest już wypożyczony");
+            return;
+        }
+        var countRents = rentedDevices.Count(r => r.RentedTo == user);
+        if ((countRents >= 2 && user.UserType == "Student") ||  (countRents >= 5 && user.UserType == "Employee"))
+        {
+            Console.WriteLine("BŁĄD: " + user.UserType + " ma już maksymalną ilość możliwych wypożyczeń");
+            return;
+        }
+        
+        device.IsAvailable = false;
+        rentedDevices.Add(new Rental(user, device, dateFrom, dateTo));
+        Console.WriteLine("Wypożyczono: " + device.Model + ", osobie: " + user.FirstName + " " + user.LastName );
+    }
     
 }
