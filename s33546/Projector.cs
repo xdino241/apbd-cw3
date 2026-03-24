f43102e38db8840375364a448777c19665bd02a5
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-24 18:08:14 +0100

    Added Projector class

---
+++ s33546/Projector.cs
@@ -0,0 +1,16 @@
+﻿namespace s33546;
+
+public class Projector : Device
+{
+    public Projector(string model, string resolution, double weight) : base("Projector")
+    {
+        Resolution = resolution;
+        Weight = weight;
+        Model = model;
+    }
+
+    public string Resolution { get;set; }
+    public double Weight { get;set;}
+    
+    public string Model { get;set;}
+}
