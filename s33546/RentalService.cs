b0ffcb1910a5c62be197a617323d37d141d3f9b6
Jakub Mikolajczyk <jakubmikolajczyk214@gmail.com>
2026-03-24 19:06:02 +0100

    Added addDevice and List of devices to RentalService

--- s33546/RentalService.cs
+++ s33546/RentalService.cs
@@ -2,5 +2,13 @@
 
 public class RentalService
 {
+    public List<Device> Devices = new List<Device>();
+
+    public void addDevice(Device device)
+    {
+        Devices.Add(device);
+        Console.WriteLine("Dodano do systemu: " +  device.Name);
+    }
+    
     
 }
